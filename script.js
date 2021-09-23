c0afdd3aa389b83353a48cc03dacdb50d2c5f67e
Egor Bokov <s1lice.egor@gmail.com>
2021-09-23 23:28:07 +0300

    Created weatherApp

--- script.js
+++ script.js
@@ -1,141 +1,102 @@
 "use strict";
-let input_city = "Moscow";
-let start_city_lon = 37.62;
-let start_city_lat = 55.75;
-let city_new_name = "Moscow";
-let city_min = 273;
-let city_max = 273;
-
-async function sendFetch() {
-
-   let resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input_city}&appid=d4d98e6d901ad75e969bd7aca1cdbef8`);
-   let data = await resp.json();
-   if (data.coord.lon) {
-      start_city_lon = data.coord.lon;
-      start_city_lat = data.coord.lat;
-      city_new_name = data.name;
-      city_min = data.main.temp_min;
-      city_max = data.main.temp_max;
-   }
-   console.log(city_new_name);
-
-   
-let mainCity = document.getElementById('city_name');
-mainCity = city_new_name;
-
-}
-
-let input = document.getElementById('city_from_input');
-button.addEventListener('click', function() {alert(input.value)});
-
-
-
-
-async function requestRealise(start_city_lon, start_city_lat) {
-let resp = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${start_city_lat}&lon=${start_city_lon}&exclude=hourly&lang=ru&appid=a9074f9b54bd255c2588b28061a0b18b`);
-
-let data = await resp.json();
-console.log(data);
-}
-
-// requestRealise();
-
 
 let date = new Date();
-let today_date = date.getDay();
-
-console.log(today_date);
-
-let days = document.getElementsByClassName('icon_day');
-console.log(days[0].innerHTML);
-
-
-let mainCity = document.getElementById('city_name');
-
-if (today_date == 0) {
-   days[0].innerHTML = 'Sun';
-   days[1].innerHTML = 'Mon';
-   days[2].innerHTML = 'Tue';
-   days[3].innerHTML = 'Wen';
-   days[4].innerHTML = 'Thu';
-   days[5].innerHTML = 'Fri';
-   days[6].innerHTML = 'Sat';
-} else if (today_date == 1) {
-   days[0].innerHTML = 'Mon';
-   days[1].innerHTML = 'Tue';
-   days[2].innerHTML = 'Wen';
-   days[3].innerHTML = 'Thu';
-   days[4].innerHTML = 'Fri';
-   days[5].innerHTML = 'Sat';
-   days[6].innerHTML = 'Sun';
-} else if (today_date == 2) {
-   days[0].innerHTML = 'Tue';
-   days[1].innerHTML = 'Wen';
-   days[2].innerHTML = 'Thu';
-   days[3].innerHTML = 'Fri';
-   days[4].innerHTML = 'Sat';
-   days[5].innerHTML = 'Sun';
-   days[6].innerHTML = 'Mon';
-} else if (today_date == 3) {
-   days[0].innerHTML = 'Wen';
-   days[1].innerHTML = 'Thu';
-   days[2].innerHTML = 'Fri';
-   days[3].innerHTML = 'Sat';
-   days[4].innerHTML = 'Sun';
-   days[5].innerHTML = 'Mon';
-   days[6].innerHTML = 'Tue';
-} else if (today_date == 4) {
-   days[0].innerHTML = 'Thu';
-   days[1].innerHTML = 'Fri';
-   days[2].innerHTML = 'Sat';
-   days[3].innerHTML = 'Sun';
-   days[4].innerHTML = 'Mon';
-   days[5].innerHTML = 'Tue';
-   days[6].innerHTML = 'Wen';
-} else if (today_date == 5) {
-   days[0].innerHTML = 'Fri';
-   days[1].innerHTML = 'Sat';
-   days[2].innerHTML = 'Sun';
-   days[3].innerHTML = 'Mon';
-   days[4].innerHTML = 'Tue';
-   days[5].innerHTML = 'Wen';
-   days[6].innerHTML = 'Thu';
-} else {
-   days[0].innerHTML = 'Sat';
-   days[1].innerHTML = 'Sun';
-   days[2].innerHTML = 'Mon';
-   days[3].innerHTML = 'Tue';
-   days[4].innerHTML = 'Wen';
-   days[5].innerHTML = 'Thu';
-   days[6].innerHTML = 'Fri';
-}
-
+let city_lat = "Moscow";
+let city_new_lat = 55.75;
+let city_new_lon = 37.61;
 
 let weekDaysArr = [
    'Sunday',
    'Monday',
    'Tuesday',
    'Wednesday',
-   'Tuesday',
+   'Thursday',
    'Friday',
    'Saturday',
 ];
 
-let shortWeekDay = document.querySelector('.week_day');
-let currentDate = document.querySelector('.today_date');
+button.addEventListener('click', () => { sendFetch(city_from_input.value) });
 
-document.getElementById('day_of_week').textContent = weekDaysArr[today_date];
+day_of_week.textContent = weekDaysArr[date.getDay()];
+week_day.textContent = weekDaysArr[date.getDay()].slice(0, 3);
+today_date.textContent = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
 
-for (let i = 0; i < weekDaysArr.length; i++) {
-   if (i == today_date) shortWeekDay.textContent = weekDaysArr[i].slice(0, 3);
+window.addEventListener('load', () => { sendFetch('Moscow') });
+
+async function sendFetch(city) {
+   let resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d4d98e6d901ad75e969bd7aca1cdbef8`);
+   let data = await resp.json();
+
+   console.log(data);
+
+   city_new_lon = Math.trunc((data.coord.lon * 100)) / 100;
+   city_new_lat = Math.trunc((data.coord.lat * 100)) / 100;
+
+   city_name.innerHTML = city;
+   main_deg_scale.textContent = `${Math.round(data.main.temp - 273)}°C`;
+   sub_deg_scale.textContent = `Feels like ${Math.round(data.main.feels_like - 273)}°C`;
+   humidity_deg.textContent = `${data.main.humidity}%`;
+   pressure.textContent = `${data.main.pressure}hPa`;
+   wind_speed.textContent = `${Math.round(data.wind.speed * 1.6)}km/h`;
+   document.getElementById('main_img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+   document.getElementById('city_from_input').value = '';
+   releaseRequest();
+   // console.log(imgSrc);
 }
 
-currentDate.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
 
-document.getElementById('button').onselectstart = new Function("return false");
+async function releaseRequest(city_lon, city_lat) {
 
-// current.feels_like
-// data.current.pressure 
-// data.current.humidity
+   let resp = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city_new_lat}&lon=${city_new_lon}&exclude=hourly&lang=ru&appid=d4d98e6d901ad75e969bd7aca1cdbef8`);
+   let data = await resp.json();
 
-sendFetch();
+   // Monday 
+
+   document.getElementById('mon').querySelector('.deg_span').textContent = `${Math.round(data.daily[0].temp.day - 273)}°C`;
+   document.getElementById('mon').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}@2x.png`;
+   document.getElementById('mon').querySelector('.sub_humidity').textContent = `${data.daily[0].humidity}%`;
+   document.getElementById('mon').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[0].wind_speed * 1.6)}km/h`;
+
+   // Tuesday
+
+   document.getElementById('tue').querySelector('.deg_span').textContent = `${Math.round(data.daily[1].temp.day - 273)}°C`;
+   document.getElementById('tue').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png`;
+   document.getElementById('tue').querySelector('.sub_humidity').textContent = `${data.daily[1].humidity}%`;
+   document.getElementById('tue').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[1].wind_speed * 1.6)}km/h`;
+
+   // Wednesday
+
+   document.getElementById('wen').querySelector('.deg_span').textContent = `${Math.round(data.daily[2].temp.day - 273)}°C`;
+   document.getElementById('wen').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png`;
+   document.getElementById('wen').querySelector('.sub_humidity').textContent = `${data.daily[2].humidity}%`;
+   document.getElementById('wen').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[2].wind_speed * 1.6)}km/h`;
+
+   // Thursday
+
+   document.getElementById('thu').querySelector('.deg_span').textContent = `${Math.round(data.daily[3].temp.day - 273)}°C`;
+   document.getElementById('thu').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}@2x.png`;
+   document.getElementById('thu').querySelector('.sub_humidity').textContent = `${data.daily[3].humidity}%`;
+   document.getElementById('thu').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[3].wind_speed * 1.6)}km/h`;
+
+   // Friday
+
+   document.getElementById('fri').querySelector('.deg_span').textContent = `${Math.round(data.daily[4].temp.day - 273)}°C`;
+   document.getElementById('fri').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}@2x.png`;
+   document.getElementById('fri').querySelector('.sub_humidity').textContent = `${data.daily[4].humidity}%`;
+   document.getElementById('fri').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[4].wind_speed * 1.6)}km/h`;
+
+   // Saturday
+
+   document.getElementById('sat').querySelector('.deg_span').textContent = `${Math.round(data.daily[5].temp.day - 273)}°C`;
+   document.getElementById('sat').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[5].weather[0].icon}@2x.png`;
+   document.getElementById('sat').querySelector('.sub_humidity').textContent = `${data.daily[5].humidity}%`;
+   document.getElementById('sat').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[5].wind_speed * 1.6)}km/h`;
+
+   // Sunday
+
+   document.getElementById('sun').querySelector('.deg_span').textContent = `${Math.round(data.daily[6].temp.day - 273)}°C`;
+   document.getElementById('sun').querySelector('.sub_main_logo').src = `https://openweathermap.org/img/wn/${data.daily[6].weather[0].icon}@2x.png`;
+   document.getElementById('sun').querySelector('.sub_humidity').textContent = `${data.daily[6].humidity}%`;
+   document.getElementById('sun').querySelector('.sub_pressure').textContent = `${Math.round(data.daily[6].wind_speed * 1.6)}km/h`;
+
+};
